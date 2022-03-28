8ee6017449d4187f7efae06f2dcd79d420a592e5
Martin Neshev <100307424+BBN-1@users.noreply.github.com>
2022-03-28 10:19:15 +0300

    Create Triangle Of Numbers.js

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/Triangle Of Numbers.js
@@ -0,0 +1,15 @@
+function solve(num){
+
+
+for (let i = 1; i <= num; i++) {
+    
+       let toPrint = ``
+       for (let j = 1; j <= i; j++) {
+          toPrint += i + ` ` 
+          
+       }
+       console.log(toPrint);
+    }
+    
+}
+
