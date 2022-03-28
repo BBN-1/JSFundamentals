54c0480ee0d8ecdf966ec705fd953f438383a1e9
Martin Neshev <100307424+BBN-1@users.noreply.github.com>
2022-03-28 10:05:09 +0300

    Create Sum Digits.js

---
+++ Data Types and Variables - Exercise/Sum Digits.js
@@ -0,0 +1,19 @@
+function solve(num) {
+
+
+    let numStr = num.toString()
+    let sum = 0;
+
+    for (let i = 0; i < numStr.length; i++) {
+        sum += Number(numStr[i])
+
+    }
+    console.log(sum)
+
+
+
+
+
+
+
+}
