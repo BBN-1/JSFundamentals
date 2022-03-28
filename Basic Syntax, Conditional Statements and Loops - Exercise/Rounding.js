178c5f33932efc0130e290626d0b59379e823605
Martin Neshev <100307424+BBN-1@users.noreply.github.com>
2022-03-28 10:16:18 +0300

    Create Rounding.js

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/Rounding.js
@@ -0,0 +1,73 @@
+function solve(num1, num2) {
+
+
+    switch (num2) {
+        case 1:
+            console.log(parseFloat((num1).toFixed(1)))
+            break;
+
+        case 2:
+            console.log(parseFloat((num1).toFixed(2)))
+            break;
+
+        case 3:
+            console.log(parseFloat((num1).toFixed(3)))
+            break;
+
+        case 4:
+            console.log(parseFloat((num1).toFixed(4)))
+            break;
+
+        case 5:
+            console.log(parseFloat((num1).toFixed(5)))
+            break;
+
+        case 6:
+            console.log(parseFloat((num1).toFixed(6)))
+            break;
+
+        case 7:
+            console.log(parseFloat((num1).toFixed(7)))
+            break;
+
+        case 8:
+            console.log(parseFloat((num1).toFixed(8)))
+            break;
+
+        case 9:
+            console.log(parseFloat((num1).toFixed(9)))
+            break;
+
+        case 10:
+            console.log(parseFloat((num1).toFixed(10)))
+            break;
+
+        case 11:
+            console.log(parseFloat((num1).toFixed(11)))
+            break;
+
+        case 12:
+            console.log(parseFloat((num1).toFixed(12)))
+            break;
+
+        case 13:
+            console.log(parseFloat((num1).toFixed(13)))
+            break;
+
+        case 14:
+            console.log(parseFloat((num1).toFixed(14)))
+            break;
+
+        case 15:
+            console.log(parseFloat((num1).toFixed(15)))
+            break;
+
+        default:
+            console.log(parseFloat((num1).toFixed(15)))
+            break;
+    }
+
+
+
+
+}
